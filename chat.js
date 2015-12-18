4cd806e7b0eb1404b34347865025803669e0b063
Chris McKnight <cmckni3@gmail.com>
2015-12-18 01:37:19 -0600

    Initial commit

---
+++ chat.js
@@ -0,0 +1,17 @@
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+  chatlog.push(message);
+});
+
+chat.on('join', function(nickname) {
+  users.push(nickname);
+});
+
+chat.emit('join', 'cmckni3');
+chat.emit('message', 'Test');
+chat.emit('mesage', 'Test2');
